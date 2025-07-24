0988b1a9ae06939d2107bdc14a68f88f9bd0dd0d
danial <nifflyar@dnl.local>
2025-07-25 03:16:55 +0500

    Changed

--- src/db/queries/create_tables.py
+++ src/db/queries/create_tables.py
@@ -3,6 +3,8 @@ import asyncio
 from src.db.database import Base, async_engine
 from src.db.config import settings
 
+from src.db.models import UsersOrm, OneTimeNewReminderOrm, DailyNewReminderOrm, DailyReminderTimes, HourlyNewReminderOrm, YearlyNewReminderOrm, WeeklyNewReminderOrm, WeeklyReminderDays, MonthlyNewReminderOrm, MonthlyReminderDays
+
 print("👉 Подключение к БД:", settings.DATABASE_URL_asyncpg) 
 
 async def create_tables():
