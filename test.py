ff25a58243de50f13407f86f7610b2d2498c89af
danial <nifflyar@dnl.local>
2025-07-25 00:13:31 +0500

    Добавлены все файлы, включая скрытые и .env

---
+++ test.py
@@ -0,0 +1,28 @@
+
+import datetime 
+from datetime import UTC
+
+# now = datetime.utcnow()
+# date_today = now.date() # "02/03/2025"
+# time_now = now.time().replace(second=0, microsecond=0)   # "14:00"
+
+# print(date_today, time_now)
+
+
+# now = datetime.datetime.now(UTC)
+# dt_combined = datetime.datetime.combine(now.date(), now.time())
+# shifted_dt = (dt_combined - datetime.timedelta(hours=int("5"))).year
+
+# timme = datetime.time(hour=15, minute=15)
+
+# dt = datetime.datetime.combine(datetime.datetime.today(), timme)
+# shifted = (dt - datetime.timedelta(hours=5)).time()
+
+# print(shifted)
+
+# s = ["h","e","l","l","o"]
+
+# for i in range(len(s)-2, -1, -1):
+#     s.append(s.pop(i))
+
+print(2//5)
